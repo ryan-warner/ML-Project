f745c8892a68c11afa266d9c435a41462e3ee2ad
Ryan Warner <ryan.warner@gatech.edu>
2024-02-19 16:37:20 -0500

    Cleaned docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './docs',
-  integrations: [tailwind()],
+  integrations: [tailwind(), relativeLinks()],
   output: 'static',
-  base: "/pages/rwarner31/ML-Project/",
-  site: "https://github.gatech.edu/pages/rwarner31/ML-Project/",
-  relative: true,
 });
