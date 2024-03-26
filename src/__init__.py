8c86f63fb1f05270409549c55ea1d75714301552
Ryan Warner <ryan.warner@gatech.edu>
2024-03-25 23:54:22 -0400

    Add data dir

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from datasets import load_dataset
 
-dataset = load_dataset("InfImagine/FakeImageDataset", streaming=True)
+dataset = load_dataset("InfImagine/FakeImageDataset", streaming=True, data_dir="/scratch")
