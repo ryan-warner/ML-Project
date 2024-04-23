df30695f3108dd718ae3718bf3011e3ebf1cccea
Huang, Weiting <weiting.huang@gatech.edu>
2024-04-23 18:57:50 -0400

    Adding cnn visualization

--- src/cnn/CNNmodel.py
+++ src/cnn/CNNmodel.py
@@ -20,6 +20,12 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+METRICS = [
+    'accuracy',
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall')
+]
+
 classifier = Sequential()
 
 classifier.add(Convolution2D(32,3,3,input_shape = (64,64,3), activation='relu'))
@@ -35,7 +41,7 @@ classifier.add(Dense(128, activation ='relu'))
 
 classifier.add(Dense(1, activation= 'sigmoid'))
 
-classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = METRICS)
 
 train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 
@@ -44,11 +50,48 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 train_set = train_datagen.flow_from_directory('dataset/train', target_size=(64,64), batch_size = 32, class_mode = 'binary')
 test_set = test_datagen.flow_from_directory('dataset/test', target_size=(64,64), batch_size = 32, class_mode = 'binary')
 
-classifier.fit(train_set, 
-              epochs=5,
+#steps = datapoints/batch_size
+r = classifier.fit(train_set, 
+              epochs=15,
               steps_per_epoch=3125,
               validation_data=(test_set), 
               validation_steps=625
               )
 
-result = classifier.predict(test_set)
+#visualization
+plt.figure(figsize=(12, 16))
+
+plt.subplot(4, 2, 1)
+plt.plot(r.history['loss'], label='Loss')
+plt.plot(r.history['val_loss'], label='val_Loss')
+plt.title('Loss Function Evolution')
+plt.legend()
+
+plt.subplot(4, 2, 2)
+plt.plot(r.history['accuracy'], label='accuracy')
+plt.plot(r.history['val_accuracy'], label='val_accuracy')
+plt.title('Accuracy Function Evolution')
+plt.legend()
+
+
+plt.subplot(4, 2, 3)
+plt.plot(r.history['precision'], label='precision')
+plt.plot(r.history['val_precision'], label='val_precision')
+plt.title('Precision Function Evolution')
+plt.legend()
+
+plt.subplot(4, 2, 4)
+plt.plot(r.history['recall'], label='recall')
+plt.plot(r.history['val_recall'], label='val_recall')
+plt.title('Recall Function Evolution')
+plt.legend()
+
+# Adjust the spacing between plots
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.9)
+
+plt.show()
