37ec13907335372e1e3fa9130953df2514892810
Ryan Warner <ryan.warner@gatech.edu>
2024-06-05 12:23:58 -0400

    [Actions] Replace tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+});
