3295b025f0d511c3ccfadcf6b6dd71ec96a7cbbe
Ryan Warner <ryan.warner@gatech.edu>
2024-06-05 12:02:55 -0400

    [Actions] Rearrange and remove GT config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,4 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  outDir: './docs',
-  integrations: [tailwind()],
-  site: 'https://github.gatech.edu',
-  base: '/pages/rwarner31/ML-Project/',
-  output: 'static',
-  build: {
-    inlineStylesheets: 'never'
-  }
-});
+export default defineConfig({});
