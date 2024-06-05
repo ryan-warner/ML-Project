c35401286302c9b46bc7163679954834fd0c84cc
Ryan Warner <ryan.warner@gatech.edu>
2024-06-05 12:33:35 -0400

    [Astro] Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/ML-Project/',
+  site: 'https://ryan-warner.github.io',
+  base: 'ML-Project',
 });
